bdefe806682a2948aa69250e34f3f46c7167a60a
LewisSim <57391605+LewisSim@users.noreply.github.com>
2019-11-13 23:51:03 +0000

    Basic State Machine FrameWork For RL Agent Added
    Added StateSystem.cs which will cover the RL agents actiosn and states

---
+++ RogueLike/Assets/Scripts/StateSystem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateSystem : MonoBehaviour
+{
+    public struct GameState
+    {
+        // TODO
+        // state definition here 
+    }
+
+    public struct GameAction
+    {
+        // TODO
+        // action definition here
+    }
+
+    public class ReinforcementProblem
+    {
+
+    }
+
+    public virtual GameState GetRandomState()
+    {
+        //TODO
+        //Define behaviour
+
+        return new GameState();
+    }
+
+    public virtual GameAction[] GetAvaliableActions(GameState s)
+    {
+        //TODO
+        //Define behaviour
+        return new GameAction[0];
+    }
+
+    public virtual GameState TakeAction(GameState s, GameAction a, ref float reward)
+    {
+        reward = 0f;
+        return new GameState();
+    }
+}
