6b32b030023ff385aace967013808bca27a5bf44
Kaisei-Fukaya <32553821+Kaisei-Fukaya@users.noreply.github.com>
2019-11-14 19:04:50 +0000

    Partially implemented level generation
    Added the grid generation code and entrance/exit placement.

---
+++ RogueLike/Assets/Scripts/LevelGen.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGen : MonoBehaviour
+{
+    GameObject masterNode;
+    public GameObject nodeRef;
+    GameObject[][] grid;
+    public int seed;
+    public int gridSizeX, gridSizeY, cellSizeX, cellSizeY;
+    int nOfCells;
+
+    public enum BorderPos { North, East, South, West };
+    public BorderPos entrancePos, exitPos;
+
+    private void Awake()
+    {
+        Random.InitState(seed);
+        //Initialise grid
+        grid = new GameObject[gridSizeX][];
+        for (int i = 0; i < grid.Length; i++)
+        {
+            grid[i] = new GameObject[gridSizeY];
+        }
+        Generate();
+    }
+
+    private void Generate()
+    {
+        GenInit();
+        GenPrim();
+    }
+
+    private void GenInit()
+    {
+        nOfCells = gridSizeX * gridSizeY;
+        Debug.Log("Gen Initialising");
+        Debug.Log("Number of Cells: " + nOfCells);
+
+        //Create master node
+        masterNode = new GameObject("Master Node");
+
+        //Generate Grid
+        int offsetX = 0;
+        int idOffset = 0;
+        for (int i = 0; i < grid.Length; i++)
+        {
+            int offsetY = 0;
+            for (int j = 0; j < grid[i].Length; j++)
+            {
+                grid[i][j] = Instantiate(nodeRef, masterNode.transform);
+                grid[i][j].transform.position = new Vector3((i + offsetX) * 10, 0, (j + offsetY) * 10);
+                var tg = grid[i][j].GetComponent<TileGenerator>();
+                tg.SetWidthHeight(cellSizeX, cellSizeY);
+                tg.iD = idOffset;
+                tg.arrayPosX = i;
+                tg.arrayPosY = j;
+                idOffset += 1;
+                offsetY += cellSizeY - 1;
+            }
+            offsetX += cellSizeX - 1;
+        }
+
+        //Place Entrance and Exit
+        PositionBorder(entrancePos, TileGenerator.RoomType.Entrance);
+        PositionBorder(exitPos, TileGenerator.RoomType.Exit);
+    }
+
+    private void GenPrim()
+    {
+
+    }
+
+
+    private void PositionBorder(BorderPos b, TileGenerator.RoomType rType)
+    {
+        switch (b)
+        {
+            case BorderPos.North:
+                //Get Target Node randomly from appropriate axis
+                var targetN = grid[Random.Range(1, grid.Length - 2)][grid[0].Length - 1].GetComponent<TileGenerator>();
+                if (!targetN.isAssigned)
+                {
+                    targetN.SetRoomType(rType);
+                }
+                else
+                {
+                    while (targetN.isAssigned == true)
+                    {
+                        targetN = grid[Random.Range(1, grid.Length - 2)][grid[0].Length - 1].GetComponent<TileGenerator>();
+                    }
+                }
+                targetN.SetRotation(-90);
+                break;
+
+            case BorderPos.East:
+                var targetE = grid[grid[0].Length - 1][Random.Range(1, grid[0].Length - 2)].GetComponent<TileGenerator>();
+                if (!targetE.isAssigned)
+                {
+                    targetE.SetRoomType(rType);
+                }
+                else
+                {
+                    while (targetE.isAssigned == true)
+                    {
+                        targetE = grid[grid[0].Length - 1][Random.Range(1, grid[0].Length - 2)].GetComponent<TileGenerator>();
+                    }
+                }
+                targetE.SetRotation(-180);
+                break;
+
+            case BorderPos.South:
+                var targetS = grid[Random.Range(1, grid.Length - 2)][0].GetComponent<TileGenerator>();
+                if (!targetS.isAssigned)
+                {
+                    targetS.SetRoomType(rType);
+                }
+                else
+                {
+                    while (targetS.isAssigned == true)
+                    {
+                        targetS = grid[Random.Range(1, grid.Length - 2)][0].GetComponent<TileGenerator>();
+                    }
+                }
+                targetS.SetRotation(-270);
+                break;
+
+            case BorderPos.West:
+                var targetW = grid[0][Random.Range(1, grid[0].Length - 2)].GetComponent<TileGenerator>();
+                if (!targetW.isAssigned)
+                {
+                    targetW.SetRoomType(rType);
+                }
+                else
+                {
+                    while (targetW.isAssigned == true)
+                    {
+                        targetW = grid[0][Random.Range(1, grid[0].Length - 2)].GetComponent<TileGenerator>();
+                    }
+                }
+                targetW.SetRotation(0);
+                break;
+        }
+    }
+
+}
---
+++ RogueLike/Assets/Scripts/TileGenerator.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGenerator : MonoBehaviour
+{
+    //NOTE: Current use of public variables is for debugging only, refactor at a later date
+    public GameObject entrance, exit, lConnect, tConnect, xConnect, sConnect, room, deadEnd;
+    public enum RoomType { Entrance, Exit, LConnect, TConnect, XConnect, SConnect, Room, DeadEnd };
+    public bool[] connectionPoints;
+    RoomType type;
+    int width, height;
+    public bool isAssigned = false;
+    public int iD;
+    public int arrayPosX, arrayPosY;
+    public bool unchangable;
+    public TileGenerator parent;
+
+
+    private void Awake()
+    {
+        //Four connection points: 0 = right, 1 = down, 2 = left, 3 = up
+        connectionPoints = new bool[4];
+
+        //Assign all to false by default
+        for (int i = 0; i < connectionPoints.Length; i++)
+        {
+            connectionPoints[i] = false;
+        }
+        unchangable = false;
+    }
+
+
+    public void SetWidthHeight(int w, int h)
+    {
+        width = w;
+        height = h;
+        int depth = width;
+        transform.localScale = new Vector3(width, height, depth);
+    }
+
+    public void SetRoomType(RoomType t)
+    {
+        type = t;
+        if (transform.childCount > 0)
+        {
+            Destroy(transform.GetChild(0).gameObject);
+        }
+        switch (type)
+        {
+            case RoomType.Entrance:
+                Instantiate(entrance, transform);
+                unchangable = true;
+                Debug.Log("Entrance spawned");
+                break;
+
+            case RoomType.Exit:
+                Instantiate(exit, transform);
+                unchangable = true;
+                Debug.Log("Exit spawned");
+                break;
+
+            case RoomType.LConnect:
+                Instantiate(lConnect, transform);
+                break;
+
+            case RoomType.TConnect:
+                Instantiate(tConnect, transform);
+                break;
+
+            case RoomType.XConnect:
+                Instantiate(xConnect, transform);
+                break;
+
+            case RoomType.SConnect:
+                Instantiate(sConnect, transform);
+                break;
+
+            case RoomType.Room:
+                Instantiate(room, transform);
+                unchangable = true;
+                break;
+
+            case RoomType.DeadEnd:
+                Instantiate(deadEnd, transform);
+                break;
+
+            default:
+                break;
+        }
+        isAssigned = true;
+    }
+
+    public void SetRotation(float r)
+    {
+        Debug.Log("WAS " + transform.rotation.y);
+        transform.rotation = Quaternion.Euler(0f, r, 0f);
+        Debug.Log(transform.rotation.eulerAngles);
+        //Assign connection point if room is pre placed type e.g. Entrance, Exit, Important room
+        if (type == RoomType.Entrance || type == RoomType.Exit || type == RoomType.Room)
+        {
+            switch (transform.rotation.y)
+            {
+                case 0:
+                    connectionPoints[0] = true;
+                    connectionPoints[1] = false;
+                    connectionPoints[2] = false;
+                    connectionPoints[3] = false;
+                    break;
+                case -90:
+                    connectionPoints[0] = false;
+                    connectionPoints[1] = true;
+                    connectionPoints[2] = false;
+                    connectionPoints[3] = false;
+                    break;
+                case -180:
+                    connectionPoints[0] = false;
+                    connectionPoints[1] = false;
+                    connectionPoints[2] = true;
+                    connectionPoints[3] = false;
+                    break;
+                case -270:
+                    connectionPoints[0] = false;
+                    connectionPoints[1] = false;
+                    connectionPoints[2] = false;
+                    connectionPoints[3] = true;
+                    break;
+
+            }
+        }
+        Debug.Log(iD + " Got Rotated: " + r);
+    }
+}
