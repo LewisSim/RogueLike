4d8151afd741c3b30e16c0d740dc42d397c82ef7
deShalom <jordanunderwoodisepic@gmail.com>
2019-11-16 19:51:05 +0000

    Added prototype populated player shop
    Prototype player shop added, with populated contents called in from XML in the resource file. Work on the Player Controller also started.

---
+++ RogueLike/Assets/Scripts/ItemBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+
+public class ItemBase 
+{
+    [XmlAttribute("paneName")]
+    public string uiPaneName;
+    [XmlElement("itmName")]
+    public string itemName;
+    [XmlElement("itmDescription")]
+    public string itemDescription;
+}
