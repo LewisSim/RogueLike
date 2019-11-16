f6f25283feb2fcd01ab9ade69980155cd7e4dd38
deShalom <jordanunderwoodisepic@gmail.com>
2019-11-16 21:27:54 +0000

    Added initial player controller prototype
    Created the initial player controller, simple walking capability so far. Working based on the input managers vertical and horizontal controls, hopefully should make it easier to change controls at a later date.

---
+++ RogueLike/Assets/Scripts/Character.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    //Variables
+    public Rigidbody rb;
+    public float movementSpeed = 3f;
+    public float jumpHeight = 15;
+
+    //Methods
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    public void Movement()
+    {
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+        //Not initially required (JAY) Vector3 verticalMovement = moveVertical * rb.transform.forward;
+        //rb.velocity = new Vector3((moveHorizontal * speed), rb.velocity.y, moveVertical * speed);
+
+        rb.velocity = new Vector3((Input.GetAxis("Horizontal") * movementSpeed), rb.velocity.y, Input.GetAxis("Vertical") * movementSpeed);
+
+    }
+
+}
