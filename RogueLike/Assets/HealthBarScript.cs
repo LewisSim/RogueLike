b0b0f4c852d4cfc51a99682afdeafeb1f4ec8151
Martyn Grazulis <mg9497u@greenwich.ac.uk>
2019-12-02 08:23:58 +0000

    HealthBar/ Scripts + OcclusionCulling Added to UI Scene
    Added a basic health bar with two scripts, so far it allows you to change the health bar values with the "Q" and "R" buttons, all the relavant values are public and easy to change.
    
    Added Occlusion culling to my UI scene, it work for the most part but the occlusion culling can be seen in the minimap; as it's only affecting he main camera.
    
    *Working on better UI designs and Concept Art.
    
    *One more bug with the footsteps needs to get fixed.

---
+++ RogueLike/Assets/HealthBarScript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour
+{
+    [SerializeField]
+    private Stat health;
+
+    [SerializeField]
+    private float fillAmount;
+
+    [SerializeField]
+    private float lerpSpeed;
+
+    [SerializeField]
+    private Image content;
+
+    [SerializeField]
+    private Color fullColor;
+
+    [SerializeField]
+    private Color lowColor;
+
+    public bool InEffect = false;
+
+    public float MaxValue { get; set; }
+
+    public float Value
+    {
+        set
+        {
+            fillAmount = Map(value, 0, MaxValue, 0, 1);
+        }
+    }
+
+    void Awake()
+    {
+        health.Initialize();
+    }
+
+
+    void Update()
+    {
+        {
+            HandleBar();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q)) // take away 10hp
+        {
+            health.CurrentVal -= 10;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R)) // add 10hp
+        {
+            health.CurrentVal += 10;
+        }
+    }
+
+    private void HandleBar()
+    {
+        if (fillAmount != content.fillAmount)
+        {
+            content.fillAmount = Mathf.Lerp(content.fillAmount, fillAmount, Time.deltaTime * lerpSpeed);
+        }
+
+        content.color = Color.Lerp(lowColor, fullColor, fillAmount);
+
+    }
+
+    private float Map(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    }
+}
+
