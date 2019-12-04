ba45cd8eeef6bc4d406a2834a123aeb9cd0a6272
Kaisei-Fukaya <32553821+Kaisei-Fukaya@users.noreply.github.com>
2019-12-04 23:43:12 +0000

    You can now "Pick-up" items
    -Left-clicking while in range of an item picks it up
    -Pick up logic currently just deletes the object- will interact with inventory system when that is implemented

---
+++ RogueLike/Assets/Scripts/ItemSystem/Pickup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public TriggerHandler detectCollider;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (detectCollider.isTriggered && detectCollider.triggeredBy.gameObject.tag == "Player")
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                PickUp();
+            }
+        }
+    }
+    void PickUp()
+    {
+        Debug.Log("You got picked up an item");
+        Destroy(gameObject.transform.parent.gameObject);
+    }
+}
