2613317d38bd6eb7f0a5825eef3ab8c1b6f08bcb
Kaisei <kjfukaya@gmail.com>
2020-02-27 19:23:51 +0000

    Basic Spike Traps with placeholder graphics
    -Spike trap that activates when stepped on and resets after a set amount of time.
    -Deals a set amount of damage.

---
+++ Assets/Scripts/Traps/SpikeTrap.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrap : MonoBehaviour
+{
+    public Vector3 untriggeredPos, triggeredPos;
+    public GameObject spikes;
+    public int damage;
+    public float timerMax;
+    private bool isTriggered;
+    private float timer = 0f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Trigger(other.transform.gameObject);
+    }
+
+    private void Update()
+    {
+        if (isTriggered)
+        {
+            if(timer < timerMax)
+            {
+                timer += Time.deltaTime;
+            }
+            if(timer >= timerMax)
+            {
+                timer = 0f;
+                isTriggered = false;
+                spikes.transform.localPosition = untriggeredPos;
+            }
+        }
+    }
+
+    public void Trigger(GameObject hit)
+    {
+        spikes.transform.localPosition = triggeredPos;
+        isTriggered = true;
+        if (hit.GetComponent<Character>())
+        {
+            hit.GetComponent<Character>().Health -= damage;
+        }
+    }
+}
