bf30187e6539f7ebd877e06f75a763daced2f402
LewisSim <57391605+LewisSim@users.noreply.github.com>
2020-05-04 16:27:26 +0100

    Added Weapons and Viking
    Added Weapons and Viking

---
+++ Assets/Scripts/FaceCameraNoYOpposite.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCameraNoYOpposite : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private GameObject obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var lookAt = player.transform.position;
+        lookAt.y = gameObject.transform.position.y;
+        transform.LookAt(player.transform);
+    }
+}
