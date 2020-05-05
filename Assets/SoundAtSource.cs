f3f57b727447cb5e34a6934a9396bde4ce43ef53
Kaisei <kjfukaya@gmail.com>
2020-05-05 17:03:29 +0100

    Added base audio system and some sounds
    -added base audio system.
    -added impact, ui and equip sounds

---
+++ Assets/SoundAtSource.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundAtSource : MonoBehaviour
+{
+    AudioSource source;
+    SoundMaster master;
+
+    public enum SoundClass { UI, MeleeImpact, RangedImpact, Equip}
+    public SoundClass sClass;
+
+    public int indexOverride;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //Add audio source if none
+        if (!gameObject.GetComponent<AudioSource>())
+        {
+            source = gameObject.AddComponent<AudioSource>();
+        }
+        else
+        {
+            source = gameObject.GetComponent<AudioSource>();
+        }
+
+        master = GameObject.FindGameObjectWithTag("SoundSystem").GetComponent<SoundMaster>();
+
+
+    }
+
+    public void TriggerSound()
+    {
+        if (sClass == SoundClass.UI)
+        {
+            master.PlaySoundAtSource(master.UISource, master.UIgeneral[indexOverride]);
+        }
+    }
+
+
+}
---
+++ Assets/SoundMaster.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundMaster : MonoBehaviour
+{
+    public AudioClip[] meleeImpacts, rangedImpacts, UIgeneral, equip;
+
+    public AudioClip[] music;
+
+    AudioSource musicSource;
+    public AudioSource UISource;
+
+    bool playing_music = false;
+
+    private void Awake()
+    {
+        musicSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+
+
+    }
+
+    public void StartMusic()
+    {
+        musicSource.Play();
+        playing_music = true;
+    }
+
+    public void StopMusic()
+    {
+        musicSource.Stop();
+        playing_music = false;
+    }
+
+    public void SetMusic(AudioClip clip)
+    {
+        musicSource.clip = clip;
+    }
+
+    public void PlaySoundAtSource(AudioSource source, AudioClip clip)
+    {
+        source.clip = clip;
+        source.Play();
+    }
+}
