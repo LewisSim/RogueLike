3cd7d6a2bbb97be1a77d7474cb943b063876806e
Kaisei <kjfukaya@gmail.com>
2020-05-08 23:45:17 +0100

    New UI Icons implemented with colours based on rarity- or for potions type

--- Assets/Scripts/Jays Scripts/Inventory.cs
+++ Assets/Scripts/Jays Scripts/Inventory.cs
@@ -40,6 +40,9 @@ public class Inventory : MonoBehaviour
     public GameObject basicItem_inspector;
     static GameObject basicItem;
 
+    public Color c_common_i, c_uncommon_i, c_rare_i, c_legendary_i, c_health_i, c_speed_i, c_damage_i;
+    static Color c_common, c_uncommon, c_rare, c_legendary, c_health, c_speed, c_damage;
+
     static Transform playerPosition;
 
     public Sprite[] icons_inspector;
@@ -65,6 +68,14 @@ public class Inventory : MonoBehaviour
         ui_slots = ui_slots_inspector;
         basicItem = basicItem_inspector;
         playerPosition = transform;
+        c_common = c_common_i;
+        c_uncommon = c_uncommon_i;
+        c_rare = c_rare_i;
+        c_legendary = c_legendary_i;
+        c_health = c_health_i;
+        c_speed = c_speed_i;
+        c_damage = c_damage_i;
+
         slotsFilled = new bool[p_inventory.Length];
     }
 
@@ -83,20 +94,7 @@ public class Inventory : MonoBehaviour
         //Check appropriate slot is empty
         if(item.Type == Item.ItemType.Armour)
         {
-            if(p_inventory[3] == null)
-            {
-                p_inventory[3] = item;
-            }
-            else
-            {
-                DropItem(3);
-                p_inventory[3] = item;
-            }
-        }
-
-        if (item.Type == Item.ItemType.Potion)
-        {
-            if (p_inventory[4] == null)
+            if(p_inventory[4] == null)
             {
                 p_inventory[4] = item;
             }
@@ -104,6 +102,19 @@ public class Inventory : MonoBehaviour
             {
                 DropItem(4);
                 p_inventory[4] = item;
+            }
+        }
+
+        if (item.Type == Item.ItemType.Potion)
+        {
+            if (p_inventory[3] == null)
+            {
+                p_inventory[3] = item;
+            }
+            else
+            {
+                DropItem(3);
+                p_inventory[3] = item;
             }
         }
 
@@ -122,19 +133,6 @@ public class Inventory : MonoBehaviour
 
         if (item.SubType == Item.ItemSubType.Spear)
         {
-            if (p_inventory[1] == null)
-            {
-                p_inventory[1] = item;
-            }
-            else
-            {
-                DropItem(1);
-                p_inventory[1] = item;
-            }
-        }
-
-        if (item.SubType == Item.ItemSubType.Bow)
-        {
             if (p_inventory[2] == null)
             {
                 p_inventory[2] = item;
@@ -143,6 +141,19 @@ public class Inventory : MonoBehaviour
             {
                 DropItem(2);
                 p_inventory[2] = item;
+            }
+        }
+
+        if (item.SubType == Item.ItemSubType.Bow)
+        {
+            if (p_inventory[1] == null)
+            {
+                p_inventory[1] = item;
+            }
+            else
+            {
+                DropItem(1);
+                p_inventory[1] = item;
             }
         }
 
@@ -214,7 +225,38 @@ public class Inventory : MonoBehaviour
 
                 ui_icon.name = p_inventory[i].GetFullName();
                 ui_icon.sprite = icons[iconType];
-                ui_icon.color = new Color(255, 255, 255, 255);
+                //Set colour based on type (pot) or tier
+                if(p_inventory[i].Type == Item.ItemType.Potion)
+                {
+                    switch (p_inventory[i].SubType)
+                    {
+                        case Item.ItemSubType.HealthPot:
+                            ui_icon.color = c_health;
+                            break;
+                        case Item.ItemSubType.DamagePot:
+                            ui_icon.color = c_damage;
+                            break;
+                        case Item.ItemSubType.SpeedPot:
+                            ui_icon.color = c_speed;
+                            break;
+                    }
+                }
+                else
+                {
+                    switch (p_inventory[i].Tier)
+                    {
+                        case Item.ItemTier.Common:
+                            ui_icon.color = c_common;
+                            break;
+                        case Item.ItemTier.Uncommon:
+                            ui_icon.color = c_uncommon;
+                            break;
+                        case Item.ItemTier.Rare:
+                            ui_icon.color = c_rare;
+                            break;
+                    }
+                }
+                ui_icon.color = new Color(ui_icon.color.r, ui_icon.color.g, ui_icon.color.b, 255);
 
                 slotsFilled[i] = true;
             }
@@ -222,7 +264,7 @@ public class Inventory : MonoBehaviour
             {
                 var ui_icon = ui_slots[i].GetComponentsInChildren<SVGImage>()[1];
                 ui_icon.sprite = null;
-                ui_icon.color = new Color(255, 255, 255, 0);
+                ui_icon.color = new Color(ui_icon.color.r, ui_icon.color.g, ui_icon.color.b, 0);
                 slotsFilled[i] = false;
             }
         }
