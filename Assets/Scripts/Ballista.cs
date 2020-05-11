56cc29d6b1f18dd36c7abc8d4ca320e5b6ba41b4
LewisSim <57391605+LewisSim@users.noreply.github.com>
2020-05-11 22:29:01 +0100

    ballista

--- Assets/Scripts/Ballista.cs
+++ Assets/Scripts/Ballista.cs
@@ -10,6 +10,23 @@ public class Ballista : MonoBehaviour
 
     public GameObject txtCanvas;
     public TextMeshProUGUI overlayText;
+    public GameObject reticle;
+    public Camera cam;
+    public GameObject character;
+    public bool usingBallista;
+    public GameObject ballista;
+
+    public float mouseSensitivity = 1;
+    public Vector2 pitchMinMax = new Vector2(-40, 85);
+    public float rotationSmoothTime = .12f;
+    Vector3 rotationSmoothVelocity;
+    Vector3 currentRotation;
+    float yaw;
+    float pitch;
+    public Vector3 aY;
+    private Vector3 velocity = Vector3.zero;
+    public GameObject dum;
+
 
     bool playerIsInBounds = false;
 
@@ -34,14 +51,65 @@ public class Ballista : MonoBehaviour
         }
     }
 
+    private void FixedUpdate()
+    {
+
+    }
+
     private void Update()
     {
         if (playerIsInBounds)
         {
-            if (Input.GetKeyDown(KeyCode.E))
+            if (Input.GetKeyDown(KeyCode.E) && (!usingBallista))
             {
+                usingBallista = true;
+                player.SetActive(false);
+                reticle.gameObject.SetActive(true);
+                cam.gameObject.SetActive(true);
+                character.SetActive(true);
+                txtCanvas.SetActive(false);
+                overlayText.GetComponent<TextMeshProUGUI>().isOverlay = true;
+            }
+        }
 
+        if (usingBallista)
+        {
+            rAttack();
+        }
+    }
+
+
+    public void rAttack() //Ranged
+    {
+        /*
+        var newY = dum.transform.localPosition.y + Input.GetAxis("Mouse Y");
+        newY = Mathf.Clamp(newY, 1f, 3.5f);
+        //aY = new Vector3(0, newY * 1, 0);
+        aY = Vector3.SmoothDamp(dum.transform.localPosition, new Vector3(dum.transform.localPosition.x, newY, dum.transform.localPosition.z), ref velocity, 0.3f);
+        dum.transform.localPosition = aY;
+        cam.transform.LookAt(dum.transform);
+
+    */
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+            
+        }
+
+    }
+    void Shoot()
+    {
+        float RattackRange = 100f;
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, RattackRange))
+        {
+            print("WorkingG");
+            //hit.collider.SendMessage("AddDamage", rangedAttackDam);
+            if (hit.transform.name == "agent")
+            {
+                //hit.collider.SendMessage("AddDamage", rangedAttackDam);
             }
         }
     }
+
 }
