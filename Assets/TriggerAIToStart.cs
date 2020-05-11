b01982ebce24f0e614a4eee12b2d5360b0033af8
Kaisei <kjfukaya@gmail.com>
2020-05-11 15:53:45 +0100

    Some changes

---
+++ Assets/TriggerAIToStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAIToStart : MonoBehaviour
+{
+    public Unit unit;
+    public EnemyLS enemy;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            unit.target = other.transform;
+            unit.enabled = true;
+            enemy.enabled = true;
+        }
+    }
+}
