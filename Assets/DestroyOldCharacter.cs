db59d9a7c619488e3d59dea704397fb7b80efc5f
Kaisei <kjfukaya@gmail.com>
2020-05-12 15:26:57 +0100

    Fixed transitions

---
+++ Assets/DestroyOldCharacter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOldCharacter : MonoBehaviour
+{
+    private void Start()
+    {
+        var chars = GameObject.FindGameObjectsWithTag("Player");
+        foreach (var item in chars)
+        {
+            if(item.gameObject != gameObject)
+            {
+                Destroy(item);
+            }
+        }
+    }
+}
